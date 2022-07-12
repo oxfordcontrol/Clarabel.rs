0fca1220a720ce19fdecb3011d4aeba70382f7e1
goulart-paul <paul.goulart@eng.ox.ac.uk>
2022-07-12 11:25:44 +0100

    update LDL filenames.  Fix QDLDL offsets

--- crates/clarabel_solver/src/core/kktsolvers/direct/quasidef/mod.rs
+++ crates/clarabel_solver/src/core/kktsolvers/direct/quasidef/mod.rs
@@ -1,21 +1,20 @@
 use clarabel_algebra::*;
-use std::ops::Range; 
 
 //ldl linear solvers kept in a submodule (not flattened) 
 pub mod ldlsolvers;
 
 //flatten direct KKT module structure
 mod datamap;
-mod directquasidefinitekktsolver;
+mod directldlkktsolver;
 mod utils;
-pub use directquasidefinitekktsolver::*;
+pub use directldlkktsolver::*;
 pub use datamap::*;
 pub use utils::*;
 
 pub trait DirectLDLSolver<T: FloatT> {
     fn update_values(&mut self, index: &[usize], values: &[T]);
     fn scale_values(&mut self, index: &[usize], scale: T);
-    fn offset_diagonal(&mut self, index: Range<usize>, values: T, signs: &[i8]);
+    fn offset_values(&mut self, index: &[usize], offset: T, signs: &[i8]);
     fn solve(&mut self, x: &mut [T], b: &[T]);
     fn refactor(&mut self, kkt: &CscMatrix<T>);
 }
