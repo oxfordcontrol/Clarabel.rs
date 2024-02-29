8e61a9e07cce671e723fbcb365fa254990c4df48
goulart-paul <paul.goulart@eng.ox.ac.uk>
2024-02-29 17:06:42 +0000

    update python example

--- examples/python/example_qp.py
+++ examples/python/example_qp.py
@@ -1,28 +1,28 @@
-import clarabel;
-import numpy as np;
-from scipy import sparse;
+import clarabel
+import numpy as np
+from scipy import sparse
 
 # Define problem data
-P = sparse.csc_matrix([[3., 0.], [0., 2.]])
+P = sparse.csc_matrix([[6., 0.], [0., 4.]])
 P = sparse.triu(P).tocsc()
 
-q = np.array([-1.,-4.])
+q = np.array([-1., -4.])
 
-A = sparse.csc_matrix( \
-    [[ 1., -2.],        #<-- LHS of equality constraint (lower bound)
-     [ 1.,  0.],        #<-- LHS of inequality constraint (upper bound)
-     [ 0.,  1.],        #<-- LHS of inequality constraint (upper bound)
-     [-1.,  0.],        #<-- LHS of inequality constraint (lower bound)
-     [ 0., -1.]]);      #<-- LHS of inequality constraint (lower bound)
+A = sparse.csc_matrix(
+    [[1., -2.],        # <-- LHS of equality constraint (lower bound)
+     [1.,  0.],        # <-- LHS of inequality constraint (upper bound)
+     [0.,  1.],        # <-- LHS of inequality constraint (upper bound)
+     [-1.,  0.],       # <-- LHS of inequality constraint (lower bound)
+     [0., -1.]])       # <-- LHS of inequality constraint (lower bound)
 
-b = np.array([0.,1.,1.,1.,1.])
+b = np.array([0., 1., 1., 1., 1.])
 
 cones = [clarabel.ZeroConeT(1), clarabel.NonnegativeConeT(4)]
 settings = clarabel.DefaultSettings()
 
-solver = clarabel.DefaultSolver(P,q,A,b,cones,settings)
+solver = clarabel.DefaultSolver(P, q, A, b, cones, settings)
 solution = solver.solve()
 print(
-    f"Solver terminated with status: {solution.status}, objective {solution.obj_val},\n"
-    f"and solution: {dict(s=solution.s, x=solution.x, z=solution.z)}"
+    f"Solver terminated with solution"
+    f"{dict(s=solution.s, x=solution.x, z=solution.z)}"
 )
