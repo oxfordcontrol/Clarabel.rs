902d4c93715cd26a345fcbc3ec0e3b2e9bb285de
goulart-paul <paul.goulart@eng.ox.ac.uk>
2025-02-03 11:45:29 +0000

    add docs

--- src/julia/mod.rs
+++ src/julia/mod.rs
@@ -1,2 +1,9 @@
+//! Clarabel.rs Julia interface.
+//!
+//! This module implements a Julia wrapper for the Rust version of Clarabel.   
+//! This module is intended for developer benchmarking and testing purposes.
+//! Most Julia users should instead using the native Julia implementation of
+//! this solver, available [here](https://github.com/oxfordcontrol/Clarabel.jl).
+
 pub(crate) mod interface;
 pub(crate) mod types;
