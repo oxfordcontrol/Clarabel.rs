006277a11abf4fd0a66d42ee878c9738227e72fa
goulart-paul <paul.goulart@eng.ox.ac.uk>
2025-03-06 13:39:32 +0000

    add docstrs

--- src/solver/core/kktsolvers/mod.rs
+++ src/solver/core/kktsolvers/mod.rs
@@ -22,10 +22,17 @@ pub trait HasLinearSolverInfo {
 }
 #[repr(C)]
 #[derive(Debug, Default, Clone)]
+/// Linear subsolver information.
+///
 pub struct LinearSolverInfo {
+    /// Name of the linear solver that was used
     pub name: String,
+    /// Number of threads used by solver
     pub threads: usize,
+    /// Whether the solver used a direct factorisation method
     pub direct: bool,
+    /// Number of nonzeros in the linear system
     pub nnzA: usize, // nnz in A for A = LDL^T
+    /// Number of nonzeros in the factored system
     pub nnzL: usize, // nnz in L for A = LDL^T
 }
